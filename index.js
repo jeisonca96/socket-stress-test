dad2f8127aca73bea01522852f2d89e47c849930
Jeison Aparicio <jeison.aparicio@logisticapp.org>
2019-03-21 16:59:42 -0500

    test

--- index.js
+++ index.js
@@ -6,59 +6,76 @@
 const io = require('socket.io-client'),
     colyseus = require('colyseus.js'),
     argv = require('./config/yargs').argv,
-    cli = require('cli')
+    cli = require('cli'),
+    date = require('date-and-time')
 
-let getInfo = async (socket) => {
+let getInfo = () => {
 
     try {
         let type_socket = argv.type
         let host_socket = argv.host
         let connections_socket = argv.connections
 
-        let time = (new Date()).getTime()
+        let timeInitial = new Date()
 
         switch (type_socket) {
             case "io":
+                ioConnection(connections_socket, host_socket)
+                
+                let timeFinal = new Date()
+                let subTime = date.subtract(timeFinal, timeInitial).toMilliseconds();
 
-                for (let i = 1; i <= connections_socket; i++) {
-                    var socket = io(host_socket)
-                    socket.on('connect', function () {
-                        var msg = 'Conectado io'
-                        cli.debug(`Connection #${i}: ${msg}`)
-                    });
-                    socket.on('event', function (data) { })
-                    socket.on('disconnect', function () {
-                        console.log('Desconectado io')
-                    });
-                }
+                console.log(`End ${connections_socket} conections in ${type_socket} in ${subTime} Milliseconds`)
 
                 break
 
             case "colyseus":
-                for (let i = 1; i <= connections_socket; i++) {
-                    var client = new colyseus.Client(host_socket)
-                    var room = client.join("ParkesColombiano", {
-                        uID: "YpUtf5q5BRjjVJN2",
-                        nickname: "ELMASTER"
-                    });
-                    var msg = 'Conectado colyseus'
-                    cli.debug(`Connection #${i}: ${msg}`)
+                colyseusConnection(connections_socket, host_socket)
 
-                    // room.onJoin.add(function () {
-                    //     console.log(client.id, "joined", room.name);
-                    // });
-                }
+                let timeFinal = new Date()
+                let subTime = date.subtract(timeFinal, timeInitial).toMilliseconds();
+
+                console.log(`End ${connections_socket} conections in ${type_socket} in ${subTime} Milliseconds`)
+
                 break
 
             default:
                 break
         }
-
-        return "info"
-
     } catch (error) {
         console.log(error)
     }
 }
 
-getInfo().then(result => console.log(result)).catch((err) => console.log(err))
+let ioConnection =  (connections_socket, host_socket) => {
+    for (let i = 1; i <= connections_socket; i++) {
+        var socket = io(host_socket)
+        socket.on('connect', function () {
+            var msg = 'Conectado io'
+            cli.debug(`Connection #${i}: ${msg}`)
+        });
+        socket.on('event', function (data) { })
+        socket.on('disconnect', function () {
+            var msg = 'Desconectado io'
+            cli.debug(`Connection #${i}: ${msg}`)
+        })
+    }
+}
+
+let colyseusConnection = (connections_socket, host_socket) => {
+    for (let i = 1; i <= connections_socket; i++) {
+        var client = new colyseus.Client(host_socket)
+        var room = client.join("ParkesColombiano", {
+            uID: "YpUtf5q5BRjjVJN2",
+            nickname: "ELMASTER"
+        });
+
+        room.onJoin.add(() => {
+            var msg = 'Conectado colyseus'
+            cli.debug(`Connection #${i}: ${msg}`)
+        });
+    }
+}
+
+// Ejecutar
+getInfo()
